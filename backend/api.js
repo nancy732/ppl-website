545ba1947d0f7d51d099cdbe79ad45205376bb95
nancy <nancyarora213@gmail.com>
2020-03-03 17:23:14 +0530

    ppl commit

--- backend/api.js
+++ backend/api.js
@@ -1,124 +1,252 @@
-var signin = require('./mongooseFile');
-var upload = require('./schema');
+var signin = require("./schema/userData");
+var upload = require("./schema/postData");
+var comment = require("./schema/comment");
 
 module.exports = {
-  user: function(data){
-      
-    return new Promise((resolve,reject)=>{
-
-        signin.findOne({email: data.email},function(err,result){
-
-            if(err)
-            {
-              console.log("error 1st")
-              reject(err)
-            }
-            if(result){
-                console.log("email exist")
-              resolve("email already exist")
-            }
-            else{
-              if(data.psw == data.repeat){
-              signin.create(data,function(err,result){
-                if(err){
-                   reject (err)
-                }
-                else{
-                  console.log(data)
-                   resolve ("successful")
-                }
-              })
-            }
-                else{
-                    resolve("enter valid Password")
-                }
-            }
-    })
-    })
-},
-
-
-users: function(data) {
-    return new Promise((resolve, reject) => 
-    {
-        signin.find({email: data.email,psw: data.psw},'email',function(err,result){
-
-          if(err){
-            reject(err)
-          }
-          else if(result.length === 1)
-          {
-            signin.findOne({email: data.email},function(err,result)
-            {
-              if(err)
-              {
-                reject (err)
+  user: function(data) {
+    return new Promise((resolve, reject) => {
+      signin.findOne({ email: data.email }, function(err, result) {
+        if (err) {
+          console.log("error 1st");
+          reject(err);
+        }
+        if (result) {
+          console.log("email exist");
+          resolve("email already exist");
+        } else {
+          if (data.psw == data.repeat) {
+            signin.create(data, function(err, result) {
+              if (err) {
+                reject(err);
+              } else {
+                console.log(data);
+                resolve("successful");
               }
-              else
-              {
-                resolve ("verify your email")
-              }
-          })      
+            });
+          } else {
+            resolve("enter valid Password");
           }
-          else{
-            resolve("Check your login details")
+        }
+      });
+    });
+  },
+
+  users: function(data) {
+    return new Promise((resolve, reject) => {
+      signin.find({ email: data.email, psw: data.psw }, "email", function(
+        err,
+        result
+      ) {
+        if (err) {
+          reject(err);
+        } else if (result.length === 1) {
+          signin.findOne({ email: data.email }, function(err, result) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve("verify your email");
+            }
+          });
+        } else {
+          resolve("Check your login details");
+        }
+      });
+    });
+  },
+  forgot: function(data) {
+    return new Promise((resolve, reject) => {
+      signin.find({ email: data.email }, "email", function(err, result) {
+        if (err) {
+          reject(err);
+        } else if (result.length === 1) {
+          resolve("change password");
+        } else {
+          resolve("Email do not exist");
+        }
+      });
+    });
+  },
+  Reset: function(data) {
+    return new Promise((resolve, reject) => {
+      if (data.psw == data.repeat) {
+        signin.update(
+          { email: data.email },
+          { $set: { psw: data.psw } },
+          function(err, result) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve("password changed");
+            }
           }
-        })
-      
-    })
-},
-forgot: function(data) {
-  return new Promise((resolve, reject) => 
-  {
-      signin.find({email: data.email},'email',function(err,result){
-
-        if(err){
-          reject(err)
-        }
-        else if(result.length === 1)
-        {
-            resolve("change password")  
-        }
-        else{
-          resolve("Email do not exist")
-        }
-      })
-    
-  })
-},
-Upload: function(data)
-{ 
-  return new Promise((resolve, reject)=>{
-    console.log("data",data)
-    upload.create(data,function(err,result){
-    if(err){
-      reject(err)
-    }
-    else{
-      console.log("post")
-      resolve(data)
-    }
-  })
-  })
-},
-Onload: function()
-{
-  return new Promise((resolve,reject)=>{
-
-    upload.find({},{_id:0},function(err,result){
-      if(err)
-      {
-        reject(err)
+        );
+      } else {
+        resolve("password did not matched");
       }
-      else{
-        console.log(result)
-        resolve(result)
-      }
-    })
-  })
-}
+    });
+  },
+  Upload: function(data) {
+    return new Promise((resolve, reject) => {
+      console.log("data", data);
+      upload.create(data, function(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("post");
+          resolve(data);
+        }
+      });
+    });
+  },
+  Onload: function() {
+    return new Promise((resolve, reject) => {
+      upload.find({}, function(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(result);
+          resolve(result);
+        }
+      });
+    });
+  },
+  Manage: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.find({ _id: data._id }, { _id: 0, like: 1, unlike: 1 }, function(
+        err,
+        result
+      ) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(result);
+          resolve(result);
+        }
+      });
+    });
+  },
+  Likes: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.find({ _id: data._id, like: data.email }, function(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("Likes ", result);
+          resolve(result);
+        }
+      });
+    });
+  },
+  ManageLikes: function(data) {
+    console.log(data);
+    return new Promise((resolve, reject) => {
+      upload.update(
+        { _id: data._id },
+        { $push: { like: data.email } },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  },
+  ManageLikesPull: function(data) {
+    console.log(data);
+    return new Promise((resolve, reject) => {
+      upload.update(
+        { _id: data._id },
+        { $pull: { like: data.email } },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  },
+  Unlikes: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.find({ _id: data._id, unlike: data.email }, function(err, result) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("Unlikes ", result);
+          resolve(result);
+        }
+      });
+    });
+  },
+  ManageUnlikesPull: function(data) {
+    console.log(data);
+    return new Promise((resolve, reject) => {
+      upload.update(
+        { _id: data._id },
+        { $pull: { unlike: data.email } },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  },
+  ManageUnlike: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.update(
+        { _id: data._id },
+        { $push: { unlike: data.email } },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  },
+  ManageComments: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.update(
+        { _id: data._id },
+        { $push: { comment: data.comment, commentMail: data.email } },
+        function(err, result) {
+          email: String;
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  },
+  ManageCommentsFind: function(data) {
+    return new Promise((resolve, reject) => {
+      upload.find(
+        { _id: data._id },
+        { _id: 0, comment: 1, commentMail: 1 },
+        function(err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(result);
+            resolve(result);
+          }
+        }
+      );
+    });
+  }
 };
-
-
-
-
--- backend/router.js
+++ backend/router.js
@@ -1,136 +1,141 @@
-var express = require("express")
-var router = express.Router()
-const passport = require("passport")
-var passportSetup = require('./config/passport-setup')
-var signin = require('./mongooseFile');
-var upload = require('./schema');
+var express = require("express");
+var router = express.Router();
+var signin = require("./schema/userData");
+var upload = require("./schema/postData");
 
-var multer = require('multer')
+var multer = require("multer");
 var storage = multer.diskStorage({
-  destination: (req, file, cb) => 
-  {
-    cb(null, '/home/com123/newProject/backend/public/profile' )
+  destination: (req, file, cb) => {
+    cb(null, "/home/com123/newProject/backend/public/profile");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   }
 });
-var upload = multer({storage: storage});
-console.log("we are on router.js")
-var userapi = require('./api');
+var upload = multer({ storage: storage });
+console.log("we are on router.js");
+var userapi = require("./api");
 
-
-router.post('/user',async function(req,res){
- data = req.body
-  try
-  {
-    let resFromAPI = await userapi.user(data); 
-    res.send(resFromAPI)
+router.post("/user", async function(req, res) {
+  data = req.body;
+  try {
+    let resFromAPI = await userapi.user(data);
+    res.send(resFromAPI);
+  } catch (err) {
+    res.send(err);
   }
-  catch(err)
-  {
-    res.send(err)
-  }
-          
-})
-
-
-router.post('/users', async function(req,res){
-  console.log("data")
-  try{
-    let resultFromAPI = await userapi.users(req.body)
-    res.send(resultFromAPI)
-  }
-  catch(err){
-    res.send(err) 
-  }
-  
-})
-
-router.post('/forgot',async function(req,res){
-    try{
-      let resultOfForgot = await userapi.forgot(req.body)
-      res.send(resultOfForgot)
-    }
-    catch(err)
-    {
-      res.send(err)
-    }
-})
-
-router.post('/upload',(req,res,next)=>{
-  next();
-},upload.single('avatar'),async function(req,res){
-  
-  try{
-    let data = req.body;
-    data.fileName = req.file.originalname;
-    let resultOfUpload = await userapi.Upload(data)
-    res.send(resultOfUpload)
-  }
-  catch(err)
-  {
-    res.send(err)
-  }
-})
-
-router.get('/onload',async function(req,res){
-  try
-  {
-    let resultOfOnload = await userapi.Onload()
-    res.send(resultOfOnload)
-  }
-  catch(err)
-  {
-    res.send(err)
-  }
-})
-
-router.get('/check',async function(req,res)
-{
-  console.log("djsdkj"+req.query.email);
-  signin.updateOne({email: req.query.email},{$set:{verifyUser:true}},function(err,result){
-    if(err)
-    {
-      res.send(err)
-    }
-    else
-    {
-      res.sendFile('/home/com123/Desktop/node demo/public/login.html')
-    }
-  })
-})
-
-router.get('/google', passport.authenticate('google',{
-  scope:['profile']
-}));
-
-router.get('/google/redirect',passport.authenticate('google'),(req,res)=>
-{
- 
-  res.redirect('/game?origin=${req.originalUrl}');
-
 });
 
-
-router.get('/game',function(req,res)
-{
-  res.sendFile('/home/com123/Desktop/node demo/public/game2.html')
-}
-)
-
-router.get('/',(req,res)=>{
-  res.sendFile('/home/com123/Desktop/node demo/public/web.html')
-})
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+router.post("/users", async function(req, res) {
+  console.log("data");
+  try {
+    let resultFromAPI = await userapi.users(req.body);
+    res.send(resultFromAPI);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-router.post('/test',function(req,res){
-  console.log(req.body);
-})
+router.post("/forgot", async function(req, res) {
+  try {
+    let resultOfForgot = await userapi.forgot(req.body);
+    res.send(resultOfForgot);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
- 
-  module.exports = router;
+router.post("/reset", async function(req, res) {
+  try {
+    console.log("data", req.body);
+    let resultOfReset = await userapi.Reset(req.body);
+    res.send(resultOfReset);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post(
+  "/upload",
+  (req, res, next) => {
+    next();
+  },
+  upload.single("avatar"),
+  async function(req, res) {
+    try {
+      let data = req.body;
+      data.date = new Date();
+      data.fileName = req.file.originalname;
+      let resultOfUpload = await userapi.Upload(data);
+      res.send(resultOfUpload);
+    } catch (err) {
+      res.send(err);
+    }
+  }
+);
+
+router.get("/onload", async function(req, res) {
+  try {
+    let resultOfOnload = await userapi.Onload();
+    res.send(resultOfOnload);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/manageLikes", async function(req, res) {
+  try {
+    console.log("managelikes", req.body);
+    let resultOfLikes = await userapi.Likes(req.body);
+    if (resultOfLikes.length === 1) {
+      console.log("email exist");
+      await userapi.ManageLikesPull(req.body);
+    } else {
+      console.log("not exist");
+      await userapi.ManageLikes(req.body);
+    }
+    let resultOfManage = await userapi.Manage(req.body);
+
+    res.send(resultOfManage);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/manageUnlike", async function(req, res) {
+  try {
+    console.log(req.body);
+    let resultOfUnlikes = await userapi.Unlikes(req.body);
+    if (resultOfUnlikes.length === 1) {
+      await userapi.ManageUnlikesPull(req.body);
+    } else {
+      await userapi.ManageUnlike(req.body);
+    }
+    let resultOfManage = await userapi.Manage(req.body);
+
+    res.send(resultOfManage);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/manageComments", async function(req, res) {
+  try {
+    await userapi.ManageComments(req.body);
+    let resultOfCommentFind = await userapi.ManageCommentsFind(req.body);
+    res.send(resultOfCommentFind);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/manageComment", async function(req, res) {
+  try {
+    let resultOfCommentFind = await userapi.ManageCommentsFind(req.body);
+    res.send(resultOfCommentFind);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+module.exports = router;
